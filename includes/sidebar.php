9ce36d0ebee1fad6f4e1131a02c3a7cb7a8f4887
Mark John <160160952+IamGotu@users.noreply.github.com>
2024-05-28 23:32:01 +0800

    adding dashboard

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -32,6 +32,14 @@ if(isset($_SESSION['auth'])) {
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+               <li class="nav-item">
+            <a href="index.php" class="nav-link">
+              <i class="nav-icon fa fa-users"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
 
           <li class="nav-item">
             <a href="Friends_List.php" class="nav-link">
@@ -42,33 +50,6 @@ if(isset($_SESSION['auth'])) {
             </a>
           </li>
           
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon far fa-envelope"></i>
-              <p>
-                Mailbox
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="pages/mailbox/mailbox.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Inbox</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/mailbox/compose.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Compose</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/mailbox/read-mail.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Read</p>
-                </a>
-              </li>
             </ul>
           </li>
         </ul>
