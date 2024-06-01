be760405f3351e3f89a19311dd8de3cd4396cbec
Mark John <markjohnjopia1@gmail.com>
2024-06-02 02:54:56 +0800

    editing some of the query

--- logincode.php
+++ logincode.php
@@ -42,7 +42,7 @@ if (isset($_POST['login_btn'])) {
             $row = mysqli_fetch_assoc($result);
 
             // Update Active status to "Active"
-            $update_sql = "UPDATE user_profile SET Active='Active' WHERE username='$username'";
+            $update_sql = "UPDATE user_profile SET Active='Active' WHERE email='$email'";
             mysqli_query($conn, $update_sql);
 
             // storing in session variable
